d6b5da5fbe4bb3a3100a7bf504845083ede42476
Jesper Kuutti <jesper.kuutti@gmail.com>
2017-12-07 05:18:51 +0200

    Ensimmäinen alkeellinen Spring sovellus ja Travis-integraatio

---
+++ src/main/java/wad/Controller/DefaultController.java
@@ -0,0 +1,18 @@
+
+package wad.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class DefaultController {
+    
+    @GetMapping("*")
+    @ResponseBody
+    public String home(){
+        return "Uutinen -> Vanhanen";
+    }
+    
+}
