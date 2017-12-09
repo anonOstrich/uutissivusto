95a85e22cb29a2b4db01a7d15768d9bc06f9a1f5
Jesper Kuutti <jesper.kuutti@gmail.com>
2017-12-10 00:54:50 +0200

    Alkeellinen lisäystoiminnallisuus. Sivu, jolla voi lisätä ja tämän post-pyynnön käsittely palvelimella. Ei mahdollisuutta lisätä kuvaa, kategorioita eikä uutisella ole kirjoittajaa

--- src/main/java/wad/controller/UutisController.java
+++ src/main/java/wad/controller/UutisController.java
@@ -1,10 +1,15 @@
 package wad.controller;
 
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import wad.domain.Article;
 import wad.repository.ArticleRepository;
 
 @Controller
@@ -19,7 +24,24 @@ public class UutisController {
         return "index";
     }
     
+    @GetMapping("/lisaa")
+    public String creation(){
+        return "creation";
+    }
     
+    @PostMapping("/uutiset")
+    public String add(@RequestParam String title, @RequestParam String lead, @RequestParam String mainText,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime published
+            ){
+        Article article = new Article(); 
+        article.setTitle(title);
+        article.setLead(lead);
+        article.setMainText(mainText);
+        article.setPublished(published);
+        articleRepository.save(article);
+        
+        return "redirect:/uutiset";
+    }
  
     
 }
