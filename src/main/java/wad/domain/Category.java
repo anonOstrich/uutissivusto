a5c7caf2e37f266d3e31673b92960b2e7ecc75bf
anonOstrich <jesper.kuutti@gmail.com>
2017-12-11 20:19:04 +0200

    Yritys fixata herokun haku kategorialla

--- src/main/java/wad/domain/Category.java
+++ src/main/java/wad/domain/Category.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 import java.util.List; 
+import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 import wad.domain.Article; 
 
@@ -17,7 +18,7 @@ import wad.domain.Article;
 @NoArgsConstructor
 public class Category extends AbstractPersistable<Long> {
     private String name; 
-    @ManyToMany(mappedBy = "categories")
+    @ManyToMany(mappedBy = "categories", fetch = FetchType.EAGER)
     private List<Article> articles;
     // Käytetään vain uutisen muokkaamisessa - uutisella olevat kategoriat on valmiiksi rastitettu 
     private boolean chosen;
